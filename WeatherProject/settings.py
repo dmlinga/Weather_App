023cb678bc61d595c51f83cd3fb554fdeacb0dc5
dmlinga <107780445+dmlinga@users.noreply.github.com>
2023-09-14 03:48:10 -0400

    Update allowed hosts
    added 'dmlinga.pythonanywhere.com'

--- WeatherProject/settings.py
+++ WeatherProject/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-zl&r3$6ifr#u8zia)ahmx%gxpa3&jp(g(hl8ukp&tq^a$$110^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['daniel01.pythonanywhere.com/']
+ALLOWED_HOSTS = ['daniel01.pythonanywhere.com/',
+                 'dmlinga.pythonanywhere.com']
 
 
 # Application definition
